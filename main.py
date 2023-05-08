137f16a881d95682ceb61a7273048f3ad4b8f98d
mandiehyewon <superheroine.won@gmail.com>
2023-05-08 17:11:16 -0400

    add 2d models, plotting

--- main.py
+++ main.py
@@ -46,20 +46,8 @@ parser.add_argument('--dataset', type=str, default=None, choices=dataset_names,
                     help="Dataset to use.")
 parser.add_argument('--model', type=str, default=None,
                     help="Model to train. Available:\n"
-                    "SVM (linear), "
-                    "SVM_grid (grid search on linear, poly and RBF kernels), "
-                    "baseline (fully connected NN), "
-                    "hu (1D CNN), "
                     "hamida (3D CNN + 1D classifier), "
-                    "lee (3D FCN), "
-                    "chen (3D CNN), "
-                    "li (3D CNN), "
-                    "he (3D CNN), "
-                    "luo (3D CNN), "
-                    "sharma (2D CNN), "
-                    "boulch (1D semi-supervised CNN), "
-                    "liu (3D semi-supervised CNN), "
-                    "mou (1D RNN)")
+                    "hamida2d (2D CNN + 1D classifier),")
 parser.add_argument('--folder', type=str, help="Folder where to store the "
                     "datasets (defaults to the current working directory).",
                     default="./Datasets/")
--- models.py
+++ models.py
@@ -39,20 +39,18 @@ def get_model(name, **kwargs):
     weights = weights.to(device)
     weights = kwargs.setdefault('weights', weights)
 
-    if name == 'nn':
-        kwargs.setdefault('patch_size', 1)
-        center_pixel = True
-        model = Baseline(n_bands, n_classes,
-                         kwargs.setdefault('dropout', False))
-        lr = kwargs.setdefault('learning_rate', 0.0001)
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-        criterion = nn.CrossEntropyLoss(weight=kwargs['weights'])
-        kwargs.setdefault('epoch', 100)
-        kwargs.setdefault('batch_size', 100)
-    elif name == 'hamida':
+    if name == 'hamida':
         patch_size = kwargs.setdefault('patch_size', 5)
         center_pixel = True
         model = HamidaEtAl(n_bands, n_classes, patch_size=patch_size)
+        lr = kwargs.setdefault('learning_rate', 0.01)
+        optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=0.0005)
+        kwargs.setdefault('batch_size', 100)
+        criterion = nn.CrossEntropyLoss(weight=kwargs['weights'])
+    elif name == 'hamida2d':
+        patch_size = kwargs.setdefault('patch_size', 5)
+        center_pixel = True
+        model = HamidaEtAl2DCNN(n_bands, n_classes, patch_size=patch_size)
         lr = kwargs.setdefault('learning_rate', 0.01)
         optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=0.0005)
         kwargs.setdefault('batch_size', 100)
@@ -129,32 +127,32 @@ class HamidaEtAl(nn.Module):
         self.patch_size = patch_size
         self.input_channels = input_channels
         dilation = (dilation, 1, 1)
-
+        
         if patch_size == 3:
             self.conv1 = nn.Conv3d(
-                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=1)
+                1, 2, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=1)
         else:
             self.conv1 = nn.Conv3d(
-                1, 20, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=0)
+                1, 2, (3, 3, 3), stride=(1, 1, 1), dilation=dilation, padding=0)
         # Next pooling is applied using a layer identical to the previous one
         # with the difference of a 1D kernel size (1,1,3) and a larger stride
         # equal to 2 in order to reduce the spectral dimension
         self.pool1 = nn.Conv3d(
-            20, 50, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
+            2, 2, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
         # Then, a duplicate of the first and second layers is created with
         # 35 hidden neurons per layer.
         self.conv2 = nn.Conv3d(
-            50, 50, (3, 3, 3), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
+            2, 2, (3, 3, 3), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
         self.pool2 = nn.Conv3d(
-            50, 75, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
+            2, 2, (3, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
         # Finally, the 1D spatial dimension is progressively reduced
         # thanks to the use of two Conv layers, 35 neurons each,
         # with respective kernel sizes of (1,1,3) and (1,1,2) and strides
         # respectively equal to (1,1,1) and (1,1,2)
         self.conv3 = nn.Conv3d(
-            75, 75, (3, 1, 1), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
+            2, 2, (3, 1, 1), dilation=dilation, stride=(1, 1, 1), padding=(1, 0, 0))
         self.conv4 = nn.Conv3d(
-            75, 75, (2, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
+            2, 2, (2, 1, 1), dilation=dilation, stride=(2, 1, 1), padding=(1, 0, 0))
 
         #self.dropout = nn.Dropout(p=0.5)
 
@@ -188,6 +186,91 @@ class HamidaEtAl(nn.Module):
         x = self.fc(x)
         return x
 
+
+class HamidaEtAl2DCNN(nn.Module):
+    """
+    3-D Deep Learning Approach for Remote Sensing Image Classification
+    Amina Ben Hamida, Alexandre Benoit, Patrick Lambert, Chokri Ben Amar
+    IEEE TGRS, 2018
+    https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=8344565
+    """
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+            init.kaiming_normal_(m.weight)
+            init.zeros_(m.bias)
+
+    def __init__(self, input_channels, n_classes, patch_size=5, dilation=1):
+        super(HamidaEtAl2DCNN, self).__init__()
+        # The first layer is a (3,3,3) kernel sized Conv characterized
+        # by a stride equal to 1 and number of neurons equal to 20
+        self.patch_size = patch_size
+        self.input_channels = input_channels
+        # dilation = (dil)
+
+        if patch_size == 3:
+            self.conv1 = nn.Conv2d(
+                200, 20, (3, 3), stride=(1, 1), dilation=dilation, padding=1)
+        else:
+            self.conv1 = nn.Conv2d(
+                200, 20, (3, 3), stride=(1, 1), dilation=dilation, padding=0)
+        # Next pooling is applied using a layer identical to the previous one
+        # with the difference of a 1D kernel size (1,1,3) and a larger stride
+        # equal to 2 in order to reduce the spectral dimension
+        self.pool1 = nn.Conv2d(
+            20, 20, (3, 1), dilation=dilation, stride=(2, 1), padding=(1, 0))
+        # Then, a duplicate of the first and second layers is created with
+        # 35 hidden neurons per layer.
+        self.conv2 = nn.Conv2d(
+            20, 75, (3, 3), dilation=dilation, stride=(1, 1), padding=(1, 0))
+        self.pool2 = nn.Conv2d(
+            75, 75, (3, 1), dilation=dilation, stride=(2, 1), padding=(1, 0))
+        # Finally, the 1D spatial dimension is progressively reduced
+        # thanks to the use of two Conv layers, 35 neurons each,
+        # with respective kernel sizes of (1,1,3) and (1,1,2) and strides
+        # respectively equal texo (1,1,1) and (1,1,2)
+        self.conv3 = nn.Conv2d(
+            75, 75, (3, 1), dilation=dilation, stride=(1, 1), padding=(1, 0))
+        self.conv4 = nn.Conv2d(
+            75, 75, (2, 1), dilation=dilation, stride=(2, 1), padding=(1, 0))
+        
+        #self.dropout = nn.Dropout(p=0.5)
+
+        self.features_size = self._get_final_flattened_size()
+        # The architecture ends with a fully connected layer where the number
+        # of neurons is equal to the number of input classes.
+        self.fc = nn.Linear(self.features_size, n_classes)
+
+        self.apply(self.weight_init)
+
+    def _get_final_flattened_size(self):
+
+        with torch.no_grad():
+            x = torch.zeros((1, self.input_channels,
+                             self.patch_size, self.patch_size))
+            x = self.pool1(self.conv1(x))
+            x = self.pool2(self.conv2(x))
+            x = self.conv3(x)
+            x = self.conv4(x)
+            print(x.shape)
+            _, c, w, h = x.size()
+        return  c * w * h
+
+    def forward(self, x):
+        x = x.squeeze()
+
+        x = F.relu(self.conv1(x))
+
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = x.view(-1, self.features_size)
+        #x = self.dropout(x)
+        x = self.fc(x)
+        return x
+    
 def train(net, optimizer, criterion, data_loader, epoch, scheduler=None,
           display_iter=100, device=torch.device('cpu'), display=None,
           val_loader=None, supervision='full'):
